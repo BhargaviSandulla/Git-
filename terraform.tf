acfdd8b48b18f0ded951fbb70246163f4a59d000
“Bhargavisandulla” <bhargavisandulla@04gmail.com>
2025-10-30 12:29:12 +0530

    main modified

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "ap-south-1"  # Mumbai region
 }
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "My-Internet-Gateway"
+  }
+}
