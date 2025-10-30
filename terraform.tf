669bc06c97407084d64c9993201bf0a6576523cb
“Bhargavisandulla” <bhargavisandulla@04gmail.com>
2025-10-30 12:23:32 +0530

    terraform modify

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "ap-south-1"  # Mumbai region
 }
+
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "My-VPC"
+  }
+}
